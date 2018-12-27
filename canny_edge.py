c920c29e7aa0b046317e63ca4b37baaca1a7b963
saanikagupta <33591235+saanikagupta@users.noreply.github.com>
2018-12-27 17:10:06 +0530

    Update canny_edge.py

--- canny_edge.py
+++ canny_edge.py
@@ -15,7 +15,7 @@ while True:
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     image = cv2.GaussianBlur(image, (5, 5), 0) 
     canny = cv2.Canny(image, 30, 150)
-    cv2.imshow("Gaussian blurred grayscale image | Canny edge detector", np.hstack([image, canny]))
+    cv2.imshow("Gaussian blurred grayscale image | Canny edge detection", np.hstack([image, canny]))
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 camera.release()
